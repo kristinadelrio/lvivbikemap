86336540e46842c2020a96b2dfdf9697ef842b5f
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2018-03-31 13:20:54 +0300

    Initial Commit

---
+++ lvivbikemap/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  lvivbikemap
+//
+//  Created by Kristina Del Rio Albrechet on 3/31/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
