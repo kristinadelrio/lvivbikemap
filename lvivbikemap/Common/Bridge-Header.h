127a9a2673e2ac46cf81781f89c2790b224b9798
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2018-04-01 11:04:37 +0300

    Added map utils

---
+++ lvivbikemap/Common/Bridge-Header.h
@@ -0,0 +1,19 @@
+//
+//  Bridge-Header.h
+//  lvivbikemap
+//
+//  Created by Kristina Del Rio Albrechet on 4/1/18.
+//
+
+#ifndef Bridge_Header_h
+#define Bridge_Header_h
+
+#import "GMUMarkerClustering.h"
+#import "GMUKMLParser.h"
+#import "GMUGeometryRenderer.h"
+#import "GMUGeoJSONParser.h"
+#import "GMUHeatmapTileLayer.h"
+#import "GMUGradient.h"
+#import "GMUWeightedLatLng.h"
+
+#endif /* Bridge_Header_h */
